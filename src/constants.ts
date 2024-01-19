dee84650ff5cc80910923345ce85d0fc6b8d3f9b
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-01-19 22:27:03 +0500

    project completed

---
+++ src/constants.ts
@@ -0,0 +1,56 @@
+export const currencyByRupee: Currency[] = [
+  {
+    name: 'DOLLAR',
+    value: 0.0036,
+    flag: '🇺🇸',
+    symbol: '$',
+  },
+  {
+    name: 'EURO',
+    value: 0.0033,
+    flag: '🇪🇺',
+    symbol: '€',
+  },
+  {
+    name: 'POUND',
+    value: 0.0028,
+    flag: '🇬🇧',
+    symbol: '£',
+  },
+  {
+    name: 'RUBEL',
+    value: 0.31,
+    flag: '🇷🇺',
+    symbol: '₽',
+  },
+  {
+    name: 'AUS DOLLAR',
+    value: 0.054,
+    flag: '🇦🇺',
+    symbol: 'A$',
+  },
+  {
+    name: 'CAN DOLLAR',
+    value: 0.0048,
+    flag: '🇨🇦',
+    symbol: 'C$',
+  },
+  {
+    name: 'YEN',
+    value: 0.53,
+    flag: '🇯🇵',
+    symbol: '¥',
+  },
+  {
+    name: 'DINAR',
+    value: 0.0011,
+    flag: '🇰🇼',
+    symbol: 'KD',
+  },
+  {
+    name: 'BITCOIN',
+    value: 0.000000088,
+    flag: '🎰',
+    symbol: '₿',
+  },
+];
